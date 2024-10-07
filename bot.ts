8d35f91829256911844e2b7d7e12265a35ed1309
Gia Phong <ngphong.7800@gmail.com>
2024-10-08 00:27:29 +0700

    bot

---
+++ bot.ts
@@ -0,0 +1,33 @@
+import { Bot } from "grammy";
+import { Menu } from "@grammyjs/menu";
+
+// Create an instance of the `Bot` class and pass your bot token to it.
+const bot = new Bot("7260846654:AAHU78ILBLd24c95PLKjpbD-4ZcVp5q4aj8"); // <-- put your bot token between the ""
+
+const menu = new Menu("my-menu-identifier")
+  .text("A", (ctx) => ctx.reply("You pressed A!"))
+  .row()
+  .text("B", (ctx) => ctx.reply("You pressed B!"));
+
+// Make it interactive
+bot.use(menu);
+// You can now register listeners on your bot object `bot`.
+// grammY will call the listeners when users send messages to your bot.
+
+// Handle the /start command.
+bot.command("start", async (ctx) => {
+  // Send the menu.
+  await ctx.reply("Check out this menu:", { reply_markup: menu });
+});
+
+bot.command("play", (ctx) =>
+  ctx.reply("Link day choi di: https://t.me/Windwebbot/fabbijsc")
+);
+// Handle other messages.
+bot.on("message", (ctx) => ctx.reply("Got another message!"));
+
+// Now that you specified how to handle messages, you can start your bot.
+// This will connect to the Telegram servers and wait for messages.
+
+// Start the bot.
+bot.start();
